cc37ced00b2f7c67830bed5c35e65a4b280b3bab
Madan gurung <gurungmadan@hotmail.com>
2022-03-24 03:45:57 +0000

    styling the form page with mui

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,22 @@
 import { Box, Button, TextField, Typography } from '@mui/material'
 import React from 'react'
+import SendIcon from '@mui/icons-material/Send';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import EmailIcon from '@mui/icons-material/Email';
+import PinDropIcon from '@mui/icons-material/PinDrop';
 
 function Contact() {
   return (
     <Box
       sx={{
-        height: "75vh",
+        height: "90vh",
         width: "100vw",
         backgroundColor: "#292E39",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        padding: "40px 0 120px 0"
+        padding: "40px 0 120px 0",
+        // border: "solid"
       }}
     >
       <Typography
@@ -24,23 +29,34 @@ function Contact() {
       <Box
         sx={{
           display: "flex",
-          margin: "100px 0 0 0"
+          margin: "100px 0 0 0",
+          // border: "solid",
+          height: "75%"
         }}
       >
         <Box
           sx={{
-            width: "25%",
+            width: "40%",
             textAlign: "center",
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
-            alignItems: "center"
+            alignItems: "center",
+            // border: "solid",
           }}
         >
-          <Box>
-            <Typography>
+          <Box
+            sx={{padding: "30px 0 30px 0"}}
+          >
+            <PhoneIphoneIcon
+              fontSize='large'
+            /><br /><br />
+            <Typography
+              variant='p'
+              sx={{color: "#88C0D0"}}
+            >
               Call Me On
-            </Typography>
+            </Typography><br /><br />
             <Typography>
               07456086580
             </Typography>
@@ -50,53 +66,96 @@ function Contact() {
               padding: "30px 0 30px 0"
             }}
           >
-            <Typography>
+            <EmailIcon
+              fontSize='large'
+            /><br /><br />
+            <Typography
+              variant='p'
+              sx={{color: "#88C0D0"}}
+            >
               Email Me At
-            </Typography>
+            </Typography><br /><br />
             <Typography>
               gurungmadan@hotmail.com
             </Typography>
           </Box>
-          <Box>
-            <Typography>
+          <Box
+            sx={{padding: "30px 0 30px 0"}}
+          >
+            <PinDropIcon
+                fontSize='large'
+              /><br /><br />
+            <Typography
+              variant='p'
+              sx={{color: "#88C0D0"}}
+            >
               Find Me At
-            </Typography>
+            </Typography><br /><br />
             <Typography>
               Reading, UK
             </Typography>
           </Box>
         </Box>
-        <Box>
-          <form>
+        <Box
+          sx={{
+            // border: "solid",
+            display: "flex",
+            // justifyContent: "space-between",
+            alignItems: "center"
+          }}
+        >
+          <Box
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 2},
+            }}
+            noValidate
+            autoComplete="off"
+          >
           <TextField 
             label="Name"
+            variant='filled'
+            focused
             sx={{
-              width: "50%",
-              // margin: "20px"
+              width: "40%",
+              // margin: "20px",
+              color: "red"
             }}
           />
           <TextField 
             label="Email"
+            variant='filled'
+            focused
             sx={{
               width: "50%"
             }}
           />
           <TextField 
             label="Subject"
-            fullWidth
+            variant='filled'
+            focused
+            sx={{
+              width: "94%"
+            }}
           />
           <TextField 
             label="Message"
-            fullWidth
             multiline
             maxRows={4}
+            focused
+            variant='filled'
+            sx={{
+              width: "94%"
+            }}
+
           />
           <Button
             variant='contained'
             sx={{color: "#2e3440", backgroundColor: "#88C0D0", height: "55px", borderRadius: "10px", fontWeight: "bold", "&:hover": {backgroundColor: "#6c99a6"}}}
             size= "large"
+            endIcon={<SendIcon />}
           >Send Message</Button>
-        </form>
+        </Box>
         </Box>
       </Box>
     </Box>
