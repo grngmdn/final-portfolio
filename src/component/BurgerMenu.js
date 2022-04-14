c4dc6e49cdc9a14bbe038b585e2fa0543e7b8f6c
Madan gurung <gurungmadan@hotmail.com>
2022-04-14 04:51:37 +0100

    fixing the hamburger menu system for the website

--- src/component/BurgerMenu.js
+++ src/component/BurgerMenu.js
@@ -1,7 +1,81 @@
-import React from 'react';
+import { Box } from '@mui/material';
+import React, { useState } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import { Divider } from '@mui/material';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { IconButton, Link, List, ListItem } from '@mui/material';
+import { links } from '../layout/Data';
 
 function BurgerMenu() {
-  return <div>BurgerMenu</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      {/* Hamburger menu and the links.
+          Hamburger menu icon using mui icons.
+      */}
+      <Box sx={{ display: { xs: 'block', md: 'none' }, marginRight: '50px' }}>
+        <IconButton>
+          <MenuIcon onClick={() => setOpen(true)} sx={{ color: '#eceff4' }} />
+        </IconButton>
+      </Box>
+
+      {/* Swipeable drawer when the hamburger menu is opened  */}
+      <SwipeableDrawer
+        open={open}
+        anchor='right'
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+        sx={{ zIndex: '2200' }}
+        PaperProps={{
+          sx: {
+            backgroundColor: '#2e3440',
+            width: '30vw',
+          },
+        }}
+      >
+        {/* Right arrow button to close the swipeable menu drawer  */}
+        <Box>
+          <IconButton>
+            <ChevronRightIcon
+              onClick={() => setOpen(false)}
+              sx={{ color: '#eceff4' }}
+            />
+          </IconButton>
+        </Box>
+
+        {/* Line divider to seperate icon above and the nav links  */}
+        <Divider />
+
+        {/* Links mapped though to list on the drawer.
+              Using <List> and <ListItem> to automatically add padding to the links. It also lists them on top of each other
+          */}
+        <List>
+          {links.map((link) => {
+            return (
+              <ListItem>
+                <Link
+                  href={link.url}
+                  key={link.id}
+                  sx={{
+                    textDecoration: 'none',
+                    color: '#eceff4',
+                    '&:hover': {
+                      backgroundColor: '#444444',
+                      borderRadius: '10px',
+                    },
+                  }}
+                >
+                  {link.text}
+                </Link>
+              </ListItem>
+            );
+          })}
+        </List>
+      </SwipeableDrawer>
+    </div>
+  );
 }
 
 export default BurgerMenu;
--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,8 +1,9 @@
 import { Link } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
+import React, { useState } from 'react';
 import { links } from './Data';
 import logo from '../img/logo.png';
+import BurgerMenu from '../component/BurgerMenu';
 
 function Nav() {
   return (
@@ -11,17 +12,32 @@ function Nav() {
         sx={{
           display: 'flex',
           justifyContent: 'space-between',
-          // border: "solid",
+          alignItems: 'center',
+          // border: 'solid',
           width: '100%',
         }}
       >
-        <Box sx={{ marginLeft: '50px' }}>
+        <Box
+          sx={{
+            marginLeft: '50px',
+            // border: 'solid',
+            display: 'flex',
+            flexGrow: '1',
+          }}
+        >
           <Link href='#home' sx={{ color: 'white', textDecoration: 'none' }}>
-            <img src={logo} width='50px' />
+            <img alt='website logo' src={logo} width='50px' />
           </Link>
         </Box>
+
         {/* looping the nav links (data.js) using map and displaying it in the page*/}
-        <Box sx={{ marginRight: '50px' }}>
+        <Box
+          sx={{
+            marginRight: '50px',
+            display: { xs: 'none', md: 'block' },
+            // border: 'solid',
+          }}
+        >
           {links.map((link) => {
             return (
               <Link
@@ -42,6 +58,7 @@ function Nav() {
             );
           })}
         </Box>
+        <BurgerMenu />
       </Box>
     </nav>
   );
